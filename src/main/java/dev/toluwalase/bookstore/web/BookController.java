805747a6b3a64764b3bc15461a8c0b5edd074d86
Toluwalase Ayoola <toluwaayo909@gmail.com>
2026-09-15 19:34:51 +0300

    finished chapter 3: task 4 & 5 added edit functionality and simple styling

--- src/main/java/dev/toluwalase/bookstore/web/BookController.java
+++ src/main/java/dev/toluwalase/bookstore/web/BookController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller 
@@ -50,6 +49,12 @@ public class BookController {
     public String deleteBook(@PathVariable("id") Long bookId) {
         bookRepository.deleteById(bookId);
         return "redirect:/booklist";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+        model.addAttribute("book", bookRepository.findById(bookId).orElse(null));
+        return "editbook";
     }
     
 }
