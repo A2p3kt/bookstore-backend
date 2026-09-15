586f8e1559013baad30837193a2c39fcb047b0c3
Toluwalase Ayoola <toluwaayo909@gmail.com>
2026-09-15 19:14:14 +0300

    finished chapter 3: task 3 CRUD

--- src/main/java/dev/toluwalase/bookstore/model/Book.java
+++ src/main/java/dev/toluwalase/bookstore/model/Book.java
@@ -77,6 +77,14 @@ public class Book {
         this.price = price;
     }
 
-    
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
     
 }
--- src/main/java/dev/toluwalase/bookstore/web/BookController.java
+++ src/main/java/dev/toluwalase/bookstore/web/BookController.java
@@ -1,10 +1,14 @@
 package dev.toluwalase.bookstore.web;
 
+import dev.toluwalase.bookstore.model.Book;
 import dev.toluwalase.bookstore.model.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller 
@@ -27,6 +31,25 @@ public class BookController {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist";
     }
-    
+
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+
+        return "addbook";
+    }
+
+    @PostMapping("/save")
+    public String saveBook(Book book) {
+        bookRepository.save(book);
+        
+        return "redirect:booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId) {
+        bookRepository.deleteById(bookId);
+        return "redirect:/booklist";
+    }
     
 }
